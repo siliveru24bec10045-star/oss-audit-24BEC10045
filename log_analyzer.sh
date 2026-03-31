5154f37209a3822c2296a99b582f4aef989ff9b2
siliveru24bec10045-star <siliveru.24bec10045@vitbhopal.ac.in>
2026-03-31 21:16:27 +0530

    Update log_analyzer.sh

--- log_analyzer.sh
+++ log_analyzer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Script 3: Disk and Permission Auditor # Author: SILIVERU SHASHANK NANDITH
+# Script 3: Disk and Permission Auditor 
+# Author: SILIVERU SHASHANK NANDITH
 
 DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
 
