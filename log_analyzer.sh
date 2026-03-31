908b9409dd229e40250908b4ca51b5ea86f98d0a
siliveru24bec10045-star <siliveru.24bec10045@vitbhopal.ac.in>
2026-03-31 21:13:20 +0530

    Create log_analyzer.sh

---
+++ log_analyzer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Script 3: Disk and Permission Auditor # Author: SILIVERU SHASHANK NANDITH
+
+DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
+
+echo "Directory Audit Report" echo "	"
+
+for DIR in "${DIRS[@]}"; do
+if [ -d "$DIR" ]; then
+PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}') SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
+echo "$DIR => Permissions: $PERMS | Size: $SIZE" else
+echo "$DIR does not exist on this system"
+fi done
+
+echo "	"
+echo "Software Config Directory Check"
+echo "	"
+
+# Change this according to your chosen software CONFIG_DIR="/etc/vlc"
+
+if [ -d "$CONFIG_DIR" ]; then
+CONFIG_INFO=$(ls -ld $CONFIG_DIR | awk '{print $1, $3, $4}') echo "$CONFIG_DIR exists => Permissions: $CONFIG_INFO"
+else
+echo "$CONFIG_DIR does NOT exist"
+fi
